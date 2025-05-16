7132839feff418dc68c73dcc88e1ab5c5f4e14b2
한희태 <cnksncks0411@naver.com>
2025-05-16 15:44:27 +0900

    최초 커밋

---
+++ src/main/kotlin/com/example/silverLife/SilverLifeApplication.kt
@@ -0,0 +1,11 @@
+package com.example.silverLife
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SilverLifeApplication
+
+fun main(args: Array<String>) {
+	runApplication<SilverLifeApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/silverLife/SilverLifeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.silverLife
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SilverLifeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
